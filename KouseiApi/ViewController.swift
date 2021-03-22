be2811b543b64d24fb9e47e8b4e4e70e8d32b34f
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-03-23 05:54:26 +0900

    Initial Commit

---
+++ KouseiApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KouseiApi
+//
+//  Created by 立岡力 on 2021/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
