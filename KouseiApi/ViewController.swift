ecb7102aad5a0cbafea40ffd08dd31d7eca2aa5a
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-03-23 06:24:07 +0900

    2021/03/23

--- KouseiApi/ViewController.swift
+++ KouseiApi/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var guideLineLabel: UILabel!
+    @IBOutlet weak var inputTextView: UITextView!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var copyButton: UIButton!
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
